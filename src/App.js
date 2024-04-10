e59e80f57bf75063f3bf2782c4e0bf1c26bc4c1b
siak631 <143899048+siak631@users.noreply.github.com>
2024-04-10 19:04:52 +0200

    images-site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from './Button';
 import ProductsList from './ProductsList';
 import Texte from './Texte';
+import Siakinours from './Siakinours';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
-      {/*ici je vais mettre un texte au dessus de l'image*/}
+      {/*ici je vais mettre un texte au dessus de l'image*/} 
         <Texte couleur='red'/>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='App-logo'>
+          <Siakinours hello='bonjour'>
+           hoho
+         </Siakinours>
+        </div>  
         <p>
           <Button />
           <ProductsList toto='red'>
-            <p>
+            
               toto
-            </p>
+            
           </ProductsList>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Siakinours.jsx
@@ -0,0 +1,11 @@
+const Siakinours = (props) => {
+
+  console.log(props.bonjour)
+return (
+  <> 
+    <img src="./images/logo-siak.png" alt="Logo Siakinours"/>
+
+  </> )
+}
+
+export default Siakinours
